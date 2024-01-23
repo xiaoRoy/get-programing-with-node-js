c8aeea19df51459532e4e7af5b3c56bdb2771eb3
Jerry Li <53632783+jerli4@users.noreply.github.com>
2024-01-23 16:45:41 +0800

    Finished a simple server.

---
+++ recipe_connection/index.js
@@ -0,0 +1,3 @@
+const cities = require("cities");
+let myCity = cities.zip_lookup("10016");
+console.log(myCity); 
---
+++ simple_server/index.js
@@ -0,0 +1,13 @@
+const http = require("http");
+const httpStatus = require("http-status-codes");
+
+const port = 3000;
+const app = http.createServer((request, response) => {
+    console.log("Received an incoming request!");
+    response.writeHead(httpStatus.OK, {"Content-Type": "text/html"});
+    let responseMessages = "<h1>Hello, Universe!</h1>";
+    response.write(responseMessages);
+    response.end();
+});
+app.listen(port);
+console.log(`The server has started is listening on port number:${port}`);
